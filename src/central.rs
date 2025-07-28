924660ac6fe600301a8fa265230cd809203bc286
Rock Boynton <rock.boynton@yahoo.com>
2025-07-27 17:22:37 -0700

    add config

--- src/central.rs
+++ src/central.rs
@@ -2,6 +2,7 @@
 #![no_std]
 
 use rmk::macros::rmk_central;
+use panic_probe as _;
 
 #[rmk_central]
 mod keybaord_central {}
--- src/peripheral.rs
+++ src/peripheral.rs
@@ -2,6 +2,7 @@
 #![no_std]
 
 use rmk::macros::rmk_peripheral;
+use panic_probe as _;
 
 #[rmk_peripheral(id = 0)]
 mod keybaord_central {}
